f845168619142cba3615698df84f0f98288bdc1b
Shahrukh Yousafzai <shahrukhyousafzaipk@gmail.com>
2026-03-20 05:13:13 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'arweave.net',
         port: '',
         pathname: '/**',
